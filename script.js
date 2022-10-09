625308bcc3b4f3f6d7ff9f691dc2255f5ebb45a0
Chetan Verma <vermachetan879@gmail.com>
2022-10-09 17:31:33 +0530

    did the onclick effect on every image

--- script.js
+++ script.js
@@ -10,3 +10,182 @@ function changeImg1() {
     }, 1000)
     
 }
+function changeImg2() {
+    let img = document.getElementById("img2")
+    let tick = document.getElementById("tick2")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg3() {
+    let img = document.getElementById("img3")
+    let tick = document.getElementById("tick3")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg4() {
+    let img = document.getElementById("img4")
+    let tick = document.getElementById("tick4")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg5() {
+    let img = document.getElementById("img5")
+    let tick = document.getElementById("tick5")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg6() {
+    let img = document.getElementById("img6")
+    let tick = document.getElementById("tick6")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg7() {
+    let img = document.getElementById("img7")
+    let tick = document.getElementById("tick7")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg8() {
+    let img = document.getElementById("img8")
+    let tick = document.getElementById("tick8")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg9() {
+    let img = document.getElementById("img9")
+    let tick = document.getElementById("tick9")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg10() {
+    let img = document.getElementById("img10")
+    let tick = document.getElementById("tick10")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg11() {
+    let img = document.getElementById("img11")
+    let tick = document.getElementById("tick11")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg12() {
+    let img = document.getElementById("img12")
+    let tick = document.getElementById("tick12")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg13() {
+    let img = document.getElementById("img13")
+    let tick = document.getElementById("tick13")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg14() {
+    let img = document.getElementById("img14")
+    let tick = document.getElementById("tick14")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg15() {
+    let img = document.getElementById("img15")
+    let tick = document.getElementById("tick15")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
+
+function changeImg16() {
+    let img = document.getElementById("img16")
+    let tick = document.getElementById("tick16")
+    img.src = `https://picsum.photos/200/200?random=${Math.random()}`;
+    tick.style.display = "block";
+    img.style.display = "none";
+    setTimeout(() => {
+        tick.style.display = "none"
+        img.style.display = "block"
+    }, 1000)
+}
